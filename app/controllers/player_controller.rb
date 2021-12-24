e42b0c1968c008c73d8474837a46d991419b00bc
therobgeorge <georg1rd@gmail.com>
2021-12-23 20:21:59 -0500

    adds player name to player object

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -5,6 +5,7 @@ class PlayerController < ApplicationController
     player = HTTP.get("https://api.sportsdata.io/v3/nba/scores/json/Players?key=#{Rails.application.credentials.api_key}").parse(:json).sample
     team = HTTP.get("https://api.sportsdata.io/v3/nba/scores/json/teams?key=#{Rails.application.credentials.api_key}").parse(:json).select{|team| team["Key"] == "#{player["Team"]}"}
     baller = {}
+    baller["name"] = player["YahooName"]
     baller["college"] = player["College"]
     baller["team_city"] = team[0]["City"]
     baller["team_name"] = team[0]["Name"]
